5dbc284f6e91036818f88b9a84a1e08511937829
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-13 21:25:22 +0530

    edit button added DOM task 14

--- main.js
+++ main.js
@@ -22,30 +22,53 @@ function onSubmit(e) {
     const userDataStringify = JSON.stringify(userData);
     localStorage.setItem(userKey, userDataStringify);
 
-
+    // create li, delete and edit elements
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
+    const editBtn = document.createElement('button');
+
+    // adding class name to elements
+    li.className = 'list-item';
+    delBtn.className = 'delete-btn delete-btn-hover';
+    editBtn.className = 'edit-btn edit-btn-hover';
+
+    // adding text node to all element
     const listTextNode = document.createTextNode(`${nameIn.value} ${emailIn.value} ${phoneIn.value}`);
     const delBtnTextNode = document.createTextNode('Delete');
+    const editBtnTextNode = document.createTextNode('Edit');
 
-    delBtn.className = 'delete-btn delete-btn-hover';
-    li.className = 'list-item';
-
+    // append textNode to specific elements
     li.appendChild(listTextNode);
     delBtn.appendChild(delBtnTextNode);
+    editBtn.appendChild(editBtnTextNode);
 
-    li.appendChild(delBtn);
+    // append element to parent element
     list.appendChild(li);
+    li.appendChild(delBtn);
+    li.appendChild(editBtn);
 
     // clear input
     nameIn.value = '';
     emailIn.value = '';
     phoneIn.value = '';
 
+    // event listener
     delBtn.addEventListener('click', deleteEl);
+    editBtn.addEventListener('click', editEl);
 
     // Delete function
     function deleteEl() {
+        li.remove();
+        localStorage.removeItem(userKey);
+    }
+
+    // Edit function
+    function editEl() {
+        const values = JSON.parse(localStorage.getItem(userKey));
+        nameIn.value = values.name;
+        emailIn.value = values.email;
+        phoneIn.value = values.phone;
+
         li.remove();
         localStorage.removeItem(userKey);
     }
