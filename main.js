790c865f5cef2a3987529ff3563f4820255e3022
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-13 15:19:32 +0530

    booking appointment app DOM task 12

---
+++ main.js
@@ -0,0 +1,29 @@
+const nameIn = document.getElementById('name');
+const emailIn = document.getElementById('email');
+const phoneIn = document.getElementById('phone');
+const myForm = document.querySelector('#my-form');
+const list = document.querySelector('.list');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    
+    const userData = {
+        name: nameIn.value,
+        email: emailIn.value,
+        phone: phoneIn.value
+    }
+    const userKey = userData.name;
+    const userDataStringify = JSON.stringify(userData);
+    localStorage.setItem(userKey, userDataStringify);
+
+    const li = document.createElement('li');
+    li.className = 'list-item';
+
+    const listTextNode = document.createTextNode(`${nameIn.value} ${emailIn.value} ${phoneIn.value}`);
+    li.appendChild(listTextNode)
+    list.appendChild(li);
+
+    li.setAttribute('style', 'text-decoration: none;')
+}
