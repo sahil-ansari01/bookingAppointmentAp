0e67d19112721db34e525f712cd7c4dc7e34990e
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-13 17:12:28 +0530

    Delete button added DOM task 13

--- main.js
+++ main.js
@@ -4,26 +4,50 @@ const phoneIn = document.getElementById('phone');
 const myForm = document.querySelector('#my-form');
 const list = document.querySelector('.list');
 
+// onSubmit event listener
 myForm.addEventListener('submit', onSubmit);
 
+// submit function 
 function onSubmit(e) {
     e.preventDefault();
-    
+    // fetch user data
     const userData = {
         name: nameIn.value,
         email: emailIn.value,
         phone: phoneIn.value
     }
+    
+    // save in localStorage
     const userKey = userData.email;
     const userDataStringify = JSON.stringify(userData);
     localStorage.setItem(userKey, userDataStringify);
 
+
     const li = document.createElement('li');
+    const delBtn = document.createElement('button');
+    const listTextNode = document.createTextNode(`${nameIn.value} ${emailIn.value} ${phoneIn.value}`);
+    const delBtnTextNode = document.createTextNode('Delete');
+
+    delBtn.className = 'delete-btn delete-btn-hover';
     li.className = 'list-item';
 
-    const listTextNode = document.createTextNode(`${nameIn.value} ${emailIn.value} ${phoneIn.value}`);
-    li.appendChild(listTextNode)
+    li.appendChild(listTextNode);
+    delBtn.appendChild(delBtnTextNode);
+
+    li.appendChild(delBtn);
     list.appendChild(li);
 
-    li.setAttribute('style', 'text-decoration: none;')
+    // clear input
+    nameIn.value = '';
+    emailIn.value = '';
+    phoneIn.value = '';
+
+    delBtn.addEventListener('click', deleteEl);
+
+    // Delete function
+    function deleteEl() {
+        li.remove();
+        localStorage.removeItem(userKey);
+    }
 }
+
